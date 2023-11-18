61cfb29d21e68984f042053f593527b7a7b58565
sanfroggy <sanuutine@gmail.com>
2023-11-19 01:06:26 +0200

    Errors fixed.

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Defining constants for bcrypt, express Router and the user module.
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
@@ -36,44 +35,3 @@ usersRouter.get('/', async (request, response) => {
 })
 
 module.exports = usersRouter
-
-=======
-//Defining constants for bcrypt, express Router and the user module.
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
-
-/*Defining the route for saving a new user with async / await, 
-and use bcrypt to hash the given password, unless
-a password is not given or it is shorter than 3 characters. */
-usersRouter.post('/', async (request, response) => {
-    const { username, password } = request.body
-
-    if (!password || password.length < 3) {
-        response.status(400).json({ error: 'Password must have a value of at least 3 characters.' }).end()
-    } else {
-        const saltRounds = 10
-        const passwordHash = await bcrypt.hash(password, saltRounds)
-
-        const user = new User({
-            username,
-            passwordHash,
-        })
-
-        const savedUser = await user.save()
-
-        response.status(201).json(savedUser)
-    }
-})
-
-/*Defining the route for getting users from MongoDB and
-populating their blogs array with the referred Blog object's
-url, title and author. */
-usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('quizzes', { title: 1, difficulty: 1, ratings: 1 })
-    response.json(users)
-})
-
-module.exports = usersRouter
-
->>>>>>> 34286c4faf3802d2bf353fa4ee84627f4c3c56f2
--- models/quiz.js
+++ models/quiz.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Defining the constants for mongoose and User.
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
@@ -57,63 +56,4 @@ quizSchema.set("toJSON", {
 
 //Export the Blog object model.
 module.exports = mongoose.model('Quiz', quizSchema)
-=======
-//Defining the constants for mongoose and User.
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 
-/*Define a schema to use as a model for a Blog object 
-to be saved to MongoDB, as well as a refence to User
-object that created the Blog object in question. */
-const quizSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Title cannot be empty'],
-        unique: [true, 'This title is already in use by another quiz. Quiz titles must be unique.']
-    },
-    difficulty: {
-        type: Number,
-        default: 0
-    },
-    highScore: {
-        type: Number,
-        default: 0
-    },
-    highScoreSetBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        default: null
-    },
-    ratings: [
-        Number
-    ],
-    questions: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Question'
-        }
-    ],
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    image: String
-})
-
-//Defining the mongoose-unique-validator plugin.
-quizSchema.plugin(uniqueValidator)
-
-/*Define the properties of the objects that are returned by the toJSON method.
-Exclude the _id value as well as the MongoDB version field __v.
-Also transform the value of _id from object to a string */
-quizSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-//Export the Blog object model.
-module.exports = mongoose.model('Quiz', quizSchema)
->>>>>>> 34286c4faf3802d2bf353fa4ee84627f4c3c56f2
